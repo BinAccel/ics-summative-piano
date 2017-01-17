98f990d0248f8a79f619cd58ce58a87996745acb
WillFlame14 <flameyanai@gmail.com>
2017-01-17 10:02:10 -0500

    Update Piano.java

--- Piano/src/piano/Piano.java
+++ Piano/src/piano/Piano.java
@@ -25,22 +25,33 @@ public class Piano extends JFrame{
     
     private void registerKeys() {
         keys.put(KeyEvent.VK_A, 60); //C
-        pianoKeys[0] = new Key(Keys.BLACK, 60);
+        pianoKeys[0] = new Key(Keys.WHITE, 60);
         keys.put(KeyEvent.VK_W, 61); //C#
+        pianoKeys[1] = new Key(Keys.BLACK, 61);
         keys.put(KeyEvent.VK_S, 62); //D
+        pianoKeys[2] = new Key(Keys.WHITE, 62);
         keys.put(KeyEvent.VK_E, 63); //D#
+        pianoKeys[3] = new Key(Keys.BLACK, 63);
         keys.put(KeyEvent.VK_D, 64); //E
+        pianoKeys[4] = new Key(Keys.WHITE, 64);
         keys.put(KeyEvent.VK_F, 65); //F
+        pianoKeys[5] = new Key(Keys.WHITE, 65);
         keys.put(KeyEvent.VK_T, 66); //F#
+        pianoKeys[6] = new Key(Keys.BLACK, 66);
         keys.put(KeyEvent.VK_J, 67); //G
+        pianoKeys[7] = new Key(Keys.WHITE, 67);
         keys.put(KeyEvent.VK_U, 68); //G#
+        pianoKeys[8] = new Key(Keys.BLACK, 68);
         keys.put(KeyEvent.VK_K, 69); //A
+        pianoKeys[9] = new Key(Keys.WHITE, 69);
         keys.put(KeyEvent.VK_I, 70); //A#
+        pianoKeys[10] = new Key(Keys.BLACK, 70);
         keys.put(KeyEvent.VK_L, 71); //B
+        pianoKeys[11] = new Key(Keys.WHITE, 71);
         keys.put(KeyEvent.VK_SEMICOLON, 72); //C
     }
     
     private HashMap<Integer, Integer>keys;
-    Key[]pianoKeys = new Key[keys.size()];;
     private Sequencer sequencer;
+    Key[]pianoKeys = new Key[12];
 }
