fabdd1c12bc1d8d6fab53664ff5190d4ce3018a3
MarkThames <markvgilbert@gmail.com>
2017-01-23 22:15:05 -0500

    idk

--- Piano/src/piano/Piano.java
+++ Piano/src/piano/Piano.java
@@ -19,7 +19,7 @@ public class Piano extends JFrame implements KeyListener{
     public Piano() {
         try{
             keys = new HashMap<>();
-            pianoKeys = new Key[13];
+            pianoKeys = new Key[128];
             registerKeys();
             setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
             setTitle("Piano");
@@ -33,23 +33,23 @@ public class Piano extends JFrame implements KeyListener{
             keyPanel = new JPanel();
             OverlayLayout ol = new OverlayLayout(keyPanel);
             keyPanel.setLayout(ol);
-            pianoKeys[5].setAlignmentX(0.0f);
-            pianoKeys[5].setAlignmentY(0.0f);
-            pianoKeys[4].setAlignmentX(0.5f);
-            pianoKeys[4].setAlignmentY(0.0f);
-            pianoKeys[3].setAlignmentX(0.0f);
-            pianoKeys[3].setAlignmentY(0.0f);
-            pianoKeys[2].setAlignmentX(0.5f);
-            pianoKeys[2].setAlignmentY(0.0f);
-            pianoKeys[1].setAlignmentX(0.5f);
-            pianoKeys[1].setAlignmentY(0.0f);
-            pianoKeys[0].setAlignmentX(1.0f);
-            pianoKeys[0].setAlignmentY(0.0f);
-            keyPanel.add(pianoKeys[1]);
-            keyPanel.add(pianoKeys[3]);
-            keyPanel.add(pianoKeys[0]);
-            keyPanel.add(pianoKeys[2]);
-            keyPanel.add(pianoKeys[4]);
+            pianoKeys[65].setAlignmentX(0.0f);
+            pianoKeys[65].setAlignmentY(0.0f);
+            pianoKeys[64].setAlignmentX(0.5f);
+            pianoKeys[64].setAlignmentY(0.0f);
+            pianoKeys[63].setAlignmentX(0.0f);
+            pianoKeys[63].setAlignmentY(0.0f);
+            pianoKeys[62].setAlignmentX(0.5f);
+            pianoKeys[62].setAlignmentY(0.0f);
+            pianoKeys[61].setAlignmentX(0.5f);
+            pianoKeys[61].setAlignmentY(0.0f);
+            pianoKeys[60].setAlignmentX(1.0f);
+            pianoKeys[60].setAlignmentY(0.0f);
+            keyPanel.add(pianoKeys[61]);
+            keyPanel.add(pianoKeys[63]);
+            keyPanel.add(pianoKeys[60]);
+            keyPanel.add(pianoKeys[62]);
+            keyPanel.add(pianoKeys[64]);
             content.add(keyPanel, BorderLayout.WEST);
             keyPanel.revalidate();
             pack();
@@ -63,8 +63,11 @@ public class Piano extends JFrame implements KeyListener{
         Piano P = new Piano();
         P.setVisible(true);
         int[][] im=ImperialMarch();
-        P.play(im, 250);
-        while(true){
+        //P.play(im, 250);
+        P.free();
+    }
+    public void free(){
+    	while(true){
         	boolean[] pres=new boolean[128];
         	for(int a=60;a<=72;a++){
     			if(noteon[a-60]){
@@ -72,14 +75,25 @@ public class Piano extends JFrame implements KeyListener{
     			}
     		}
         	for(int a=0;a<128;a++){
-        		if(pres[a]) mc[0].noteOn(a, 100000);
-        		else mc[0].noteOff(a);
+        		if(pres[a]){
+        			mc[0].noteOn(a, 100);
+        			if(!pianoKeys[a].isPressed()){
+    					pianoKeys[a].press();
+    				}
+        		}
+        		else{
+        			mc[0].noteOff(a);
+        			if(pianoKeys[a].isPressed()){
+    					pianoKeys[a].depress();
+    				}
+        		}
         	}
+        	keyPanel.revalidate();
         	psit(100);
         }
     }
     public static int[][] ImperialMarch(){
-    	int[][] s1=new int[10000][128];
+    	int[][] s1=new int[100][128];
     	for(int a=0;a<128;a++){
     		num[a]=0;
     	}
@@ -107,13 +121,17 @@ public class Piano extends JFrame implements KeyListener{
         add(s1, 79, 152, 7);
         add(s1, 78, 160, 5);
         add(s1, 77, 166, 2);
-        add(s1, 76, 168, 7);
-        add(s1, 68, 176, 7);
+        add(s1, 76, 168, 2);
+        add(s1, 75, 170, 2);
+        add(s1, 76, 172, 4);
+        add(s1, 68, 179, 4);
         add(s1, 73, 184, 7);
         add(s1, 72, 192, 5);
         add(s1, 71, 198, 2);
-        add(s1, 70, 200, 7);
-        add(s1, 63, 208, 7);
+        add(s1, 70, 200, 2);
+        add(s1, 69, 202, 2);
+        add(s1, 70, 204, 4);
+        add(s1, 63, 211, 4);
         add(s1, 66, 216, 7);
         add(s1, 63, 224, 5);
         add(s1, 66, 230, 2);
@@ -125,32 +143,27 @@ public class Piano extends JFrame implements KeyListener{
     }
     private void registerKeys() {
     	for(int a=67;a<=72;a++) side[a]=1;
+    	for(int a=0;a<128;a++){
+    		if(a%12==0||a%12==2||a%12==4||a%12==5||a%12==7||a%12==9||a%12==11){
+    			pianoKeys[a] = new Key(Keys.WHITE, a);
+    		}
+    		else{
+    			pianoKeys[a] = new Key(Keys.BLACK, a);
+    		}
+    	}
         keys.put(KeyEvent.VK_A, 60); //C
-        pianoKeys[0] = new Key(Keys.WHITE, 60);
         keys.put(KeyEvent.VK_W, 61); //C#
-        pianoKeys[1] = new Key(Keys.BLACK, 61);
         keys.put(KeyEvent.VK_S, 62); //D
-        pianoKeys[2] = new Key(Keys.WHITE, 62);
         keys.put(KeyEvent.VK_E, 63); //D#
-        pianoKeys[3] = new Key(Keys.BLACK, 63);
         keys.put(KeyEvent.VK_D, 64); //E
-        pianoKeys[4] = new Key(Keys.WHITE, 64);
         keys.put(KeyEvent.VK_F, 65); //F
-        pianoKeys[5] = new Key(Keys.WHITE, 65);
         keys.put(KeyEvent.VK_T, 66); //F#
-        pianoKeys[6] = new Key(Keys.BLACK, 66);
         keys.put(KeyEvent.VK_J, 67); //G
-        pianoKeys[7] = new Key(Keys.WHITE, 67);
         keys.put(KeyEvent.VK_U, 68); //G#
-        pianoKeys[8] = new Key(Keys.BLACK, 68);
         keys.put(KeyEvent.VK_K, 69); //A
-        pianoKeys[9] = new Key(Keys.WHITE, 69);
         keys.put(KeyEvent.VK_I, 70); //A#
-        pianoKeys[10] = new Key(Keys.BLACK, 70);
         keys.put(KeyEvent.VK_L, 71); //B
-        pianoKeys[11] = new Key(Keys.WHITE, 71);
         keys.put(KeyEvent.VK_SEMICOLON, 72); //C
-        pianoKeys[12] = new Key(Keys.WHITE, 72);
     }
     private HashMap<Integer, Integer>keys;
     private Synthesizer synthesizer ;
@@ -166,23 +179,26 @@ public class Piano extends JFrame implements KeyListener{
     			}
     			if(on[a]){
     				mc[0].noteOn(a, 100);
-    				if(!pianoKeys[keyMod(a)].isPressed()) pianoKeys[keyMod(a)].press();
-    				keyPanel.revalidate();
+    				if(!pianoKeys[a].isPressed()){
+    					pianoKeys[a].press();
+    				}
     			}
     			else{
     				mc[0].noteOff(a);
-    				if(pianoKeys[keyMod(a)].isPressed()) pianoKeys[keyMod(a)].depress();
-    				keyPanel.revalidate();
+    				if(pianoKeys[a].isPressed()){
+    					pianoKeys[a].depress();
+    				}
     			}
     		}
+			keyPanel.revalidate();
     		psit(100);
     	}
     }
-    
+    /*
     private int keyMod(int i) {
     	return ((i - 60) % pianoKeys.length + pianoKeys.length) % pianoKeys.length;
     }
-    
+    */
     public static void add(int[][] aa, int ind, int start, int len){
     	aa[num[ind]][ind]=start;
     	num[ind]++;
@@ -201,7 +217,6 @@ public class Piano extends JFrame implements KeyListener{
     public void keyPressed(KeyEvent evt) {
         if(keys.containsKey(evt.getKeyCode())) {
         noteon[keys.get(evt.getKeyCode()) - 60]=true;
-        pianoKeys[keys.get(evt.getKeyCode()) - 60].press();
         }
         else if(evt.getKeyCode()==KeyEvent.VK_C||evt.getKeyCode()==KeyEvent.VK_V){
                 if(evt.getKeyCode()==KeyEvent.VK_C&&!check[0]){
@@ -228,7 +243,6 @@ public class Piano extends JFrame implements KeyListener{
     public void keyReleased(KeyEvent evt) {
 		if(keys.containsKey(evt.getKeyCode())) {
 			noteon[keys.get(evt.getKeyCode()) - 60]=false;
-			pianoKeys[keys.get(evt.getKeyCode()) - 60].depress();
 		}
 		else if(evt.getKeyCode()==KeyEvent.VK_C||evt.getKeyCode()==KeyEvent.VK_V||evt.getKeyCode()==KeyEvent.VK_N||evt.getKeyCode()==KeyEvent.VK_M){
 			if(evt.getKeyCode()==KeyEvent.VK_C) check[0]=false;
